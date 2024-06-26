cf73dc1fe47c7fa48348e46de7acd5e225b2d2d0
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2024-06-26 15:21:23 +0200

    sixth commit

--- some_pkg/__init__.py
+++ some_pkg/__init__.py
@@ -1,5 +1,5 @@
-from .some_func import add_nums, mult_nums
+from .some_func import add_nums, multiply_nums
 from .another_func import print_something
 
-__all__ = [  "mult_nums", "add_nums", "print_something" ]
+__all__ = [  "multiply_nums", "add_nums", "print_something" ]
 
--- some_pkg/some_func.py
+++ some_pkg/some_func.py
@@ -3,6 +3,6 @@ import numpy as np
 def add_nums(arr: np.ndarray):
     return np.sum(arr)
 
-def mult_nums(arr: np.ndarray):
+def multiply_nums(arr: np.ndarray):
     return np.prod(arr)
 
