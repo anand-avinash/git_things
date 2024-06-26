7470b7bdcdc583bcbfe5ce4c68625dd96c52f1be
Avinash Anand <36325275+anand-avinash@users.noreply.github.com>
2024-06-26 15:14:50 +0200

    third commit

--- some_pkg/__init__.py
+++ some_pkg/__init__.py
@@ -1,4 +1,5 @@
 from .some_func import add_nums
+from .another_func import print_something
 
-__all__ = [ "add_nums" ]
+__all__ = [ "add_nums", "print_something" ]
 
---
+++ some_pkg/another_func.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def print_something(whateverobject):
+    print(whateverobject)
+
